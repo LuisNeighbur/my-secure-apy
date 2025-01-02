f7ffecbeb3b3b1451b8280ece80ff8b179a85d05
Luis Neighbur <luisneighbur@gmail.com>
2025-01-02 05:06:34 -0300

    fix cluster

--- terraform/eks_cluster.tf
+++ terraform/eks_cluster.tf
@@ -6,15 +6,29 @@ resource "aws_eks_cluster" "eks_cluster" {
   vpc_config {
     subnet_ids              = aws_subnet.private[*].id
     endpoint_private_access = true
-    endpoint_public_access  = true
+    endpoint_public_access  = false
     security_group_ids      = [aws_security_group.cluster.id]
   }
 
   enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+
+  encryption_config {
+    provider {
+      key_arn = aws_kms_key.eks.arn
+    }
+    resources = ["secrets"]
+  }
 
   depends_on = [
     aws_iam_role_policy_attachment.eks_cluster_policy,
     aws_iam_role_policy_attachment.eks_service_policy,
     aws_cloudwatch_log_group.eks_cluster
   ]
+}
+
+
+resource "aws_kms_key" "eks" {
+  description             = "EKS Secret Encryption Key"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
 }
